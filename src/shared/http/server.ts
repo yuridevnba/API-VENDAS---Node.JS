dc93c19f131c05fb8a4dea43cb2d3831c488469f
Yuri12 <yuri.00000830267@unicap.br>
2023-03-23 10:20:53 -0300

    Rotas de Produtos,Validação dos dados de requisição(celebrate)

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
 //console.log('Hello Dev!');// para rodar precisa fazer o buil compilar o arquivo para js.
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from './erros/AppError';
 import '@shared/http/typeorm';
@@ -15,9 +17,12 @@ app.use(express.json()); // a api vai trabalhar com o padrão json.
 app.use(routes);
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 
+app.use(errors()); // se for gerado o erro na rotas, o erro vai ser enviado para aplicação, por aqui, pega o erro e trata logo em baixo.
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) { // erro gerado pela nossa aplicação sempre vai cair aqui.
+    if (error instanceof AppError) {
+      // erro gerado pela nossa aplicação sempre vai cair aqui.
       return response.status(error.statusCode).json({
         status: 'error',
         message: error.message,
